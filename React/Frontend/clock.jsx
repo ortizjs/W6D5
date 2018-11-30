559ef7e2c2cac5248fb152bf8a5060ece4a6e1f7
ortizjs <jonnatano94@gmail.com>
2018-11-30 15:18:38 -0800

    Add actual date

--- React/Frontend/clock.jsx
+++ React/Frontend/clock.jsx
@@ -6,6 +6,7 @@ class Clock extends React.Component {
     this.state = {
       time: new Date()
     };
+    console.log(this.state.time);
     this.interval = 0;
     this.tick = this.tick.bind(this);
   }
@@ -22,13 +23,28 @@ class Clock extends React.Component {
   }
 
   render () {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     let hours = this.state.time.getHours();
     let minutes = this.state.time.getMinutes();
     let seconds = this.state.time.getSeconds();
+    let day = this.state.time.getDay();
+    let date = this.state.time.getDate();
+    let month = this.state.time.getMonth();
+    let year = this.state.time.getYear();
     return (
       <div>
-        <h1 className ='clock'>Clock</h1>
-        <p className ='curtime'>{hours}:{minutes}:{seconds}</p>
+        Clock
+        <div>
+          <div>
+            <h1 className ='clock'>Time:</h1>
+            <p className ='curtime'>{hours}:{minutes}:{seconds}</p>
+          </div>
+          <div>
+            <h1 className ='clock'>Date:</h1>
+            <p className ='curtime'>{days[day]} {months[month]} {date} {year + 1900}</p>
+          </div>
+        </div>
       </div>
     );
 
