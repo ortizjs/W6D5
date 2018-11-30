4da77227da4c57fc72b6ae4d5a0b33fd291392fd
ortizjs <jonnatano94@gmail.com>
2018-11-30 14:12:43 -0800

    Add Clock widget

---
+++ React/Frontend/clock.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor (props){
+    super (props);
+    this.state.time = new Date();
+    this.interval = 0;
+  }
+
+  componentDidMount () {
+    this.interval = setInterval(tick, 1000);
+  }
+  componentWillUnmount () {
+    clearInterval(this.interval);
+  }
+
+  tick () {
+    this.setState ({time: new Date()});
+  }
+
+  render () {
+    let hours = this.state.time.getHours();
+    let minutes = this.state.time.getMinutes();
+    let seconds = this.state.time.getSeconds();
+    return (
+      <div>
+      <h1>Clock</h1>
+      <span>{hours}:{minutes}:{seconds}</span>
+    </div>
+  );
+
+  }
+}
+
+
+
+export default Clock;
--- React/widgets.jsx
+++ React/widgets.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './Frontend/clock';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<Widget/>, root);
+    ReactDOM.render(<Clock/>, root);
 });
