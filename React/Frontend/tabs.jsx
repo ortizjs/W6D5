d5f96265e55484352a77fd17c3105ce5a97caa6d
ortizjs <jonnatano94@gmail.com>
2018-11-30 16:39:57 -0800

    Add Tabs initial phase

--- React/Frontend/tabs.jsx
+++ React/Frontend/tabs.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 class Tab extends React.Component {
-  
+  constructor (props){
+    super (props);
+    this.state = {
+      tabs_index: 0
+    };
+    // props.values
+  }
+
+  render(){
+    return (
+
+    );
+  }
 }
 
-export default Tabs;
+Tab.Header = class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                Tab Header
+            </div>
+        );
+    }
+}
+
+export default Tab;
--- React/Frontend/widgets.jsx
+++ React/Frontend/widgets.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
+const Root = () => (
+  <div>
+    <Clock/>
+    <Tab values={[{one: "I am the first tab"}, {two: "I am the second tab"}, {three: "I am the third tab"}]}/>
+  </div>
+);
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<Clock/>, root);
+    ReactDOM.render(<Root/>, root);
 });
